07b403c909fe08b6376acc81687102df0f55c443
Wellsx <stefanveleusic@hotmail.com>
2023-06-26 18:45:46 +0200

    fix(fileUpload): add File.separator to make test platform independent

--- src/test/java/alerts/FileUploadTests.java
+++ src/test/java/alerts/FileUploadTests.java
@@ -3,14 +3,18 @@ package alerts;
 import base.BaseTests;
 import org.testng.annotations.Test;
 
+import java.io.File;
+
 import static org.testng.Assert.assertEquals;
 
 public class FileUploadTests extends BaseTests {
 
     @Test
-    public void testFileUpload(){
+    public void testFileUpload() {
         var uploadPage = homePage.clickFileUpload();
-        uploadPage.uploadFile(System.getProperty("user.dir")+ "\\resources\\cat.jpg");
+        String filePath = System.getProperty("user.dir") + File.separator + "resources" + File.separator + "cat.jpg";
+        uploadPage.uploadFile(filePath);
         assertEquals(uploadPage.getUploadedFiles(), "cat.jpg", "Failed upload.");
     }
+
 }
